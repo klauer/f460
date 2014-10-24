3c6fa611d5e5eb90fddf1051258fda1bbdc8b91d
Ken L <klauer@bnl.gov>
2014-10-24 14:10:50 -0400

    Autosave filename fix

--- iocBoot/iocf460/autosave.cmd
+++ iocBoot/iocf460/autosave.cmd
@@ -1,4 +1,4 @@
-# autosave/restore mechanisms
+# Autosave/restore
 save_restoreSet_Debug(0)
 save_restoreSet_IncompleteSetsOk(1)
 save_restoreSet_DatedBackupFiles(1)
@@ -9,10 +9,11 @@ system "install -m 0766 -d $(TOP)/as/save/"
 set_savefile_path("${TOP}/as","/save")
 set_requestfile_path("${TOP}/as","/req")
 
+set_pass0_restoreFile("info_settings.sav")
+set_pass0_restoreFile("info_positions.sav")
+
 cd $(TOP)/as/req
 makeAutosaveFiles()
 cd ${TOP}/iocBoot/${IOC}/
-
-set_pass1_restoreFile("F460.sav")
 
 
